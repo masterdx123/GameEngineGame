eb4a13f86a67d40c455cf336fe6ebfdd009b413d
Tiago Patrício <72805135+masterdx123@users.noreply.github.com>
2023-03-29 06:13:16 +0100

    need fixes json read

--- GameEngine/GameEngineProject/GameEngineProject/UISubsystem.cpp
+++ GameEngine/GameEngineProject/GameEngineProject/UISubsystem.cpp
@@ -21,13 +21,19 @@ UISubsystem::UISubsystem(std::string path_, GameEngine* gameEngine_) : path(path
 
 		gameEngine->gameObjects->push_back(new GameObject(obj["name"].asString()));
 
+		std::cout << obj["name"].asString() << std::endl;
 		GraphicsSubsystem* tempptr = static_cast<GraphicsSubsystem*>(gameEngine->subsystems->at(0));
+
+		std::vector<Vector2> enemyWaypoints = { Vector2(1.0f, -1.0f), Vector2(3.0f,-1.0f) };
+
+		std::cout << obj["GraphicsComponent"].asBool() << std::endl;
+		std::cout << obj["IOComponent"].asBool() << std::endl;
 
 		if (obj["GraphicsComponent"].asBool()) {
 			BoxShape2D* playerBoxShape = new BoxShape2D(gameEngine->gameObjects->back(), gameEngine->subsystems->at(0), tempptr->GetWindow(), Vector2(obj["posX"].asFloat(), obj["posY"].asFloat()), Vector2(obj["scaleX"].asFloat(), obj["scaleY"].asFloat()), obj["texture"].asString());
 			gameEngine->gameObjects->back()->AddComponent(gameEngine->subsystems->at(0)->AddComponent(playerBoxShape));
-		}
-		if (obj["PhysicsComponent"].asBool()) {
+		}	
+		/*if (obj["PhysicsComponent"].asBool()) {
 			PhysicsComponent* playerPhysics = new PhysicsComponent(gameEngine->gameObjects->back(), gameEngine->subsystems->at(1), obj["halfWidth"].asFloat());
 			gameEngine->gameObjects->back()->AddComponent(gameEngine->subsystems->at(1)->AddComponent(playerPhysics));
 		}
@@ -35,19 +41,15 @@ UISubsystem::UISubsystem(std::string path_, GameEngine* gameEngine_) : path(path
 			IOComponent* playerIO = new IOComponent(gameEngine->gameObjects->back(), gameEngine->subsystems->at(2));
 			gameEngine->gameObjects->back()->AddComponent(gameEngine->subsystems->at(2)->AddComponent(playerIO));
 		}
-		if (obj["IOComponent"].asBool())
+		if (obj["AIComponent"].asBool())
 		{
-			AIComponent* enemyAI = new AIComponent(gameEngine->gameObjects->back(), gameEngine->subsystems->at(3),  /* colocar no json */ enemyWaypoints);
+			AIComponent* enemyAI = new AIComponent(gameEngine->gameObjects->back(), gameEngine->subsystems->at(3), enemyWaypoints);
+			gameEngine->gameObjects->back()->AddComponent(gameEngine->subsystems->at(3)->AddComponent(enemyAI));
 		}		
 		if (obj["NetComponent"].asBool()) {
 			NetComponent* player1Network = new NetComponent(gameEngine->gameObjects->back(), gameEngine->subsystems->at(4));
 			gameEngine->gameObjects->back()->AddComponent(gameEngine->subsystems->at(4)->AddComponent(player1Network));
-		}
-	
-		
-			
-		
-		
-		
+		}*/
+				
 	}
 }
